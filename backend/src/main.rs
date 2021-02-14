7d787dbc22422bca0386970de9ed4e71c840c44e
Tarik Eshaq <tarikeshaq@gmail.com>
2021-02-13 22:51:44 -0800

    Adds back database pool manager

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,9 +2,10 @@ use actix_web::{get, middleware, App, Error, HttpResponse, HttpServer};
 
 #[macro_use]
 extern crate diesel;
-// use diesel::prelude::*;
-// use diesel::r2d2::ConnectionManager;
+use diesel::prelude::*;
+use diesel::r2d2::ConnectionManager;
 mod schema;
+
 #[get("/")]
 async fn hello_world() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().body("Hello world!"))
@@ -16,19 +17,18 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
     dotenv::dotenv().ok();
 
-    //TODO: Add this when enabling postgres
-    // let connspec = std::env::var("DATABASE_URL").expect("Database url not provided");
-    // let manager = ConnectionManager::<PgConnection>::new(connspec);
-    // let pool = r2d2::Pool::builder()
-    //     .build(manager)
-    //     .expect("Failed to create database pool");
+    let connspec = std::env::var("DATABASE_URL").expect("Database url not provided");
+    let manager = ConnectionManager::<PgConnection>::new(connspec);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create database pool");
 
     let bind = std::env::var("BIND").expect("bind string not provided, should be given as IP:PORT");
     println!("Starting server at: {}", bind);
 
     HttpServer::new(move || {
         App::new()
-            //TODO: Add back when enabling postgres .data(pool.clone())
+            .data(pool.clone())
             .wrap(middleware::Logger::default())
             .service(hello_world)
         //TODO: Add services here
