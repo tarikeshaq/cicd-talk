12f84461d952169eec156018ec0ea0badba930a1
Tarik Eshaq <tarikesh@student.ubc.ca>
2021-03-20 19:47:19 -0700

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to launch pad <a href="https://nextjs.org">Next.js!</a>
+          Welcome to launch pad ci/cd talk <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
